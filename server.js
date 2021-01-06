b59c650774b39ae603326d4c0c8027c949c22c9d
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2021-01-06 20:02:19 +0200

    use weather API to get data

--- server.js
+++ server.js
@@ -2,18 +2,41 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
+/* Dependencies */
+const bodyParser = require('body-parser');
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors()); 
 
-// Initialize the main project folder
+//Initialize the main project folder
 app.use(express.static('website'));
 
+// Listen Port
+const port = '3000';
 
+// Spin up the server
+const server = app.listen(port, ()=>{console.log(`runing on localhost: http://localhost:${port}`)})
+
+// Get All Data
+app.get('/all', (req, res)=> {
+    res.send(projectData);
+});
+
+// Post Data
+app.post('/postData', (req, res)=> {
+    projectData = {
+        temp: req.body.temp,
+        date: req.body.date,
+        content: req.body.content
+    };
+     res.send(projectData);
+});
 // Setup Server
--- website/app.js
+++ website/app.js
@@ -1,5 +1,102 @@
-/* Global Variables */
+/*******************************  
+*****   Global Variables   *****
+********************************/
+const apiKey = '&appid=2143c3b648fa6801136a86ca8d608a79&units=imperial',
+      apiURL = 'http://localhost:3000',
+      zip = document.getElementById('zip'),
+      feelings = document.getElementById('feelings'),
+      date = document.getElementById('date'),
+      temp = document.getElementById('temp'),
+      content = document.getElementById('content'),
+      generateBtn = document.getElementById('generate');
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+/****************************** 
+*****   GLOBAL FUNCTION   *****
+*******************************/
+//1- Function to GET Web API Data
+const getWeather = async (zipCode)=> {
+    const request =  await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}${apiKey}`)
+        try {
+            const data = await request.json()
+            return data;
+        }
+        catch(err) {
+            console.log('Error', err);
+        }
+}
+
+//2- Function to POST request 
+const postData = async (url='', data={})=> {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+        try {
+            const newData = await response.json();
+            return newData;
+        }
+        catch(err) {
+            console.log('Error', err);
+        }
+}
+
+//3- Function to update the DOM
+const updateUI = async ()=> {
+    const req = await fetch('/all');
+        try {
+            const allData = await req.json();
+                date.innerHTML = 'Date : '+ allData.date;
+                temp.innerHTML = 'Tempreture : '+ allData.temp;
+                // Alert the User if the feeling box was empty & just return the date & the tempreture
+                if(allData.content == '') {
+                    return alert('You missed a feelings box')
+                }else{
+                    content.innerHTML = 'Your Feelings : '+ allData.content;
+                }
+        }
+        catch(err) {
+            console.log('Error : ', err);
+        }
+}
+
+//4- Function to retrieve data on click generate button
+const generateData = ()=> {
+    // Scroll the page to the bottom
+    window.scrollTo(0,document.body.scrollHeight);
+    // Get data from API 
+    getWeather(zip.value).then((data)=>{
+        // If the User didn't enter the zipCode
+        if(data.cod == 400){
+            return  alert(data.message);
+        // If the User entered wrong zipCode
+        }else if (data.cod == 404) {
+            return  alert(data.message);
+        }else {
+            // Post data to the server
+            postData('/postData', {
+                temp: data.main.temp,
+                date: d.toLocaleDateString(),
+                content: feelings.value
+            }).then(
+            // Get data from the server
+            updateUI()
+            )         
+        }
+    });
+}
+
+/****************************** 
+*****   EVENT LISTENER   *****
+*******************************/
+// Add Event Listener for Generate Button to Retrieve Data 
+generateBtn.addEventListener('click', generateData);
+   
